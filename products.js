beab5ad9fdb126ee99a62b97b51eb1c090544688
ZEPTA3210 <zepta.devrep@zepta.com.pk>
2025-10-13 20:22:25 +0500

    Updated homepage or styles

---
+++ products.js
@@ -0,0 +1,73 @@
+let PD = document.querySelector('#all-products');
+console.log(PD);
+
+function showSkeletons(count = 9) {
+  PD.innerHTML = ""; 
+  for (let i = 0; i < count; i++) {
+    const skeleton = document.createElement("div");
+    skeleton.classList.add("skeleton-card");
+    skeleton.innerHTML = `
+      <div class="skeleton skeleton-img"></div>
+      <div class="skeleton skeleton-text"></div>
+      <div class="skeleton skeleton-price"></div>
+    `;
+    PD.appendChild(skeleton);
+  }
+}
+
+const clothingCategories = [
+  'mens-shirts',
+  'mens-shoes',
+  'mens-watches',
+  'womens-dresses',
+  'womens-shoes',
+  'womens-jewellery'
+];
+
+const APIcall = async () => {
+  try {
+     showSkeletons(24)
+    let allClothing = [];
+
+    for (let category of clothingCategories) {
+      let response = await fetch(`https://dummyjson.com/products/category/${category}`);
+      let data = await response.json();
+      allClothing = allClothing.concat(data.products);
+    }
+
+    console.log("🧥 Fashion Collection:", allClothing);
+   PD.innerHTML = "";
+    
+    allClothing.forEach((product) => {
+      let product_card = document.createElement('div');
+      product_card.classList.add('product-card');
+
+      product_card.innerHTML = `
+        <img class="product-img" src="${product.thumbnail}" alt="${product.title}">
+        <h2>${product.title}</h2>
+        <p class="price">$${product.price}</p>
+        <button class="btn"><a href="productDetail.html"class="btn.a">Check Detail</a></button>
+      `;
+
+      const button = product_card.querySelector(".btn")
+button.addEventListener ("click", ()=> {
+localStorage.setItem ("product-detail");
+  window.location.href= "productDetail.html";
+});
+
+      PD.appendChild(product_card);
+    });
+
+  } catch (error) {
+    let errorMessage = document.createElement('h1');
+    errorMessage.innerText = "404 — Products Not Found";
+    PD.appendChild(errorMessage);
+    console.log(error);
+  }
+};
+
+
+
+
+APIcall();
+
